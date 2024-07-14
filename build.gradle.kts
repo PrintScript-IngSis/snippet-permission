ba8979f5ea0801f92381e6a85238442af9dcbcfc
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-07-13 22:59:54 -0300

    fix docker connections

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.postgresql:postgresql:42.3.1")  // Add this line for PostgreSQL
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 

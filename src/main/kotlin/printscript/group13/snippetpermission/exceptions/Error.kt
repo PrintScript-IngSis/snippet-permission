eb794121ecf0c1896e8d7e944709a7d3ac5915b4
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-07-19 19:24:16 -0300

    relic

--- src/main/kotlin/printscript/group13/snippetpermission/exceptions/Error.kt
+++ src/main/kotlin/printscript/group13/snippetpermission/exceptions/Error.kt
@@ -11,5 +11,3 @@ class PermissionAlreadyExistsException() : RuntimeException("Permission already 
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 class PermissionInvalidException(value: String) : RuntimeException("Permission $value is invalid")
-
-//nada que pushear

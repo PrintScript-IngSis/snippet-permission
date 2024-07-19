33bc917ee5ca25e5629b4c9ee18597f73e0c697e
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2024-07-19 04:24:17 -0300

    merge conflicts + lint + wildcards

--- src/main/kotlin/printscript/group13/snippetpermission/controller/PermissionController.kt
+++ src/main/kotlin/printscript/group13/snippetpermission/controller/PermissionController.kt
@@ -3,7 +3,14 @@ package printscript.group13.snippetpermission.controller
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import printscript.group13.snippetpermission.input.PermissionInput
 import printscript.group13.snippetpermission.model.Permission
 import printscript.group13.snippetpermission.service.PermissionService
@@ -39,7 +46,7 @@ class PermissionController(
         @PathVariable userId: String,
         @Valid @RequestBody permissionInput: PermissionInput,
     ): ResponseEntity<Permission> {
-        val permission = permissionService.updatePermission(snippetId, userId, permissionInput.permission!!)
+        val permission = permissionService.updatePermission(snippetId, userId, permissionInput.permission)
         return ResponseEntity.ok().body(permission)
     }
 
